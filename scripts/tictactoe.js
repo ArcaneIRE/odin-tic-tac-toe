103f1e123ee6340bfe7563183c270d709653efb2
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-07-08 19:33:33 +0100

    Add gameBoard module

---
+++ scripts/tictactoe.js
@@ -0,0 +1,25 @@
+const gameBoard = (() => {
+    const emptyBoard = ['', '', '', '', '', '', '', '', ''];
+    let board = emptyBoard;
+    const boardElem = document.querySelector('#board');
+
+    const renderBoard = () =>  {
+        boardElem.innerHTML = '';
+        board.forEach(cell => {
+            boardElem.innerHTML += `<div>${cell}</div>`;
+        });
+    };
+    const makeMove = (player, boardIndex) => {
+        board[boardIndex] = player;
+        renderBoard();
+    };
+    const resetBoard = () => {
+        board = emptyBoard;
+        renderBoard();
+    };
+    const getBoard = () => {
+        return board;
+    };
+
+    return {renderBoard, makeMove, resetBoard, getBoard};
+})();
